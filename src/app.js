6cc62f29d6ddcd63b408939fff7335ffd1265a00
arbi abeshi <arbilse@gmail.com>
2019-04-19 17:55:29 -0400

    get firebase expenses on start

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
-import { addExpense } from './actions/expenses';
+import { startSetExpenses } from './actions/expenses';
 import { setTextFilter } from './actions/filters';
 import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
@@ -12,7 +12,6 @@ import './styles/styles.scss';
 import 'react-dates/initialize'; //https://github.com/airbnb/react-dates#initialize
 import 'react-dates/lib/css/_datepicker.css'; // old version
 import './firebase/firebase';
-// import './playground/promises';
 
 
 const store = configureStore();
@@ -23,4 +22,8 @@ const jsx = (
   </Provider>
 );
 
-ReactDOM.render(jsx, document.getElementById('app'));
+ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
+
+store.dispatch(startSetExpenses()).then(() => {
+  ReactDOM.render(jsx, document.getElementById('app'));
+});
